05eb310ece4baf65e9682989727520d287d2d387
aliskair <aliskairraul@hotmail.com>
2024-08-19 12:22:07 -0400

    Configurando nuevamente Procfile y añadiendo puertos en app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import dash
 from dash import Dash, html, dcc
+import os
 
 
 app = Dash(
@@ -28,4 +29,5 @@ app.layout = html.Div(
 )
 
 if __name__ == "__main__":
-    app.run_server(debug=False)
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(debug=False, host="0.0.0.0", port=port)
