4d71e2f82a179fa180aec60dfffdd12f6a658bc7
aliskair <aliskairraul@hotmail.com>
2024-08-19 12:51:01 -0400

    Volviendo a configuracin inicial

--- app.py
+++ app.py
@@ -29,5 +29,6 @@ app.layout = html.Div(
 )
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8050))
-    app.run_server(debug=False, host="0.0.0.0", port=port)
+    app.run_server(debug=False)
+    # port = int(os.environ.get("PORT", 8050))
+    # app.run_server(debug=False, host="0.0.0.0", port=port)
